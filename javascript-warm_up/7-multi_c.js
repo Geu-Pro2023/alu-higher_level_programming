3308d58bc7a389ac82c3cafb87c4d6a264406c6b
myUsername <g.bior@alustudent.com>
2023-09-26 10:21:05 -0700

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
-let num = process.argv[2];
+const numOccurrences = process.argv[2];
 
-if (!isNaN(parseInt(process.argv[2]))) {
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
+if (!isNaN(parseInt(numOccurrences))) {
+  const count = parseInt(numOccurrences);
+  
+  if (count > 0) {
+    for (let i = 0; i < count; i++) {
+      console.log("C is fun");
+    }
+  } else {
+    console.log("Missing number of occurrences");
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log("Missing number of occurrences");
 }
