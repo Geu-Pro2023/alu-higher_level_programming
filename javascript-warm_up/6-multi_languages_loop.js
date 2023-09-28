6809ad1b5761dc129051a8a88c45d8ea1afcb789
myUsername <g.bior@alustudent.com>
2023-09-28 07:47:15 -0700

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
-}
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+myVar.forEach((element) => {
+  console.log(element);
+});
